fa4d770e2602c27b96ce9507524043f5b70e7c73
drplx <drplix065@gmail.com>
2025-07-06 19:56:17 +0700

    new json

--- internal/model/task.go
+++ internal/model/task.go
@@ -1,11 +1,15 @@
 package model
 
+import (
+	"time"
+)
+
 type Task struct {
-	ID          string `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	CreatedAt   string `json:"created_at"`
-	UpdatedAt   string `json:"updated_at"`
-	DoneAt      string `json:"done_at"`
-	Completed   bool   `json:"completed"`
+	ID          string     `db:"id" json:"id"`
+	Title       string     `db:"title" json:"title"`
+	Description string     `db:"description" json:"description"`
+	CreatedAt   time.Time  `db:"created_at" json:"created_at"`
+	UpdatedAt   time.Time  `db:"updated_at" json:"updated_at"`
+	DoneAt      *time.Time `db:"done_at" json:"done_at,omitempty"`
+	Completed   bool       `db:"completed" json:"completed"`
 }
